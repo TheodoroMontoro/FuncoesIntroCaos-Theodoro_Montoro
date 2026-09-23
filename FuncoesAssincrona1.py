d4949ca877cb7e99c5f12ad501d3c747f19124fd
TheodoroMontoro <theodoromontoro@gmail.com>
2026-09-22 23:53:02 -0300

    Modify functions for new map adaptation
    Updated functions to adapt to the new map used in exercises 3 and 4, including modifications to the Lyapunov function and the plotting function.

--- FuncoesAssincrona1.py
+++ FuncoesAssincrona1.py
@@ -1,17 +1,20 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def Mapa(x0, b, n=1, nT=0):
+np.random.seed(15489998) # Meu nUSP como seed de aleatoriedade
+
+def Mapa(x0, b, n=1, nT=0): # Funções do diretório do professor, levemente modificadas para se adequarem ao mapa utilizado nos exercícios 3 e 4
   x = x0
   X = []
   for i in range(-nT,n):
-    x = (1-(b*(x**2)))
+    x = (1-(b*(x**2))) # Mapa de nUSP par
     if i >= 0:
       X.append(x)
   return np.asarray(X)
 #
 
-def Lyapunov( X, b, fPrimeMinimo=1e-10 ):
+def Lyapunov( X, b, fPrimeMinimo=1e-10 ): # Levemente alterado para se adequar ao novo mapa considerado
   L = np.zeros_like(b)
   for x in X:
     fPrime = np.maximum( np.abs(2*b*x), fPrimeMinimo )
@@ -23,7 +26,7 @@ def FazFiguraDiagramaBifurcacao( Bs, X, L, \
               figsize = None, alpha = 0.2, ylimL = None, \
               corL = 'b', corX = 'k', \
               SHOW = True, DEVOLVE_fig_axs = False, \
-              fig = None, axs=None ):
+              fig = None, axs=None ): # Levemente alterado para se adequar ao novo mapa considerado. Já é a versão atualizada que leva o ContadorPeriodo em consideração
   if fig is None:
     if figsize is not None:
       fig = plt.figure(figsize=figsize)
@@ -41,7 +44,7 @@ def FazFiguraDiagramaBifurcacao( Bs, X, L, \
 
   # separar entre periódico e caótico
   #axs[0].plot( As, X.T, ',k', alpha=alpha )
-  Ps = contadorPeriodo( X );
+  Ps = contadorPeriodo( X ); 
   ondePeriodico = np.flatnonzero( Ps>0 )
   ondeCaotico = np.flatnonzero( Ps==0 )
   axs[0].plot( Bs[ondePeriodico], X[:,ondePeriodico].T, ',', c=corX, alpha=1 )
@@ -62,7 +65,7 @@ def FazFiguraDiagramaBifurcacao( Bs, X, L, \
 #
 
 
-def contadorPeriodo( X, delta=1e-5, pMax=np.inf, nVerMin=0 ):
+def contadorPeriodo( X, delta=1e-5, pMax=np.inf, nVerMin=0 ): # Mesmo contador de período feito para as atividades em sala
   if X.ndim==1:
     X = np.atleast_2d(X).T
   Ps = []
